6bf5e89501485f33424a079942d99e8b4a67d9af
nameanonymous <misamasa19960226@gmail.com>
2025-03-31 13:23:46 +0200

    Some modification

--- results_app/controller/mainpage_controller.py
+++ results_app/controller/mainpage_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, jsonify
+from flask import Blueprint, request, render_template, jsonify, url_for
 from flask_restful import Api, Resource, abort
 import os
 sagami_bp = Blueprint('sagami_bp', __name__)
@@ -10,6 +10,16 @@ def show_main_homepage():
         dir_path = os.path.dirname(os.path.realpath(__file__))
         print(dir_path)
         return render_template("HTML/main.html")
+    except AttributeError as e:
+        logging.error(f"endpoint={request.path} method={request.method}, error={str(e)}")
+        return jsonify({'error': str(e)}), 400
+
+
+@sagami_bp.route('/menu', methods=['GET'], strict_slashes=False)
+def show_menu_pdf():
+    try:
+        print(url_for('static', filename='pdf/menu.pdf'))
+        return render_template("HTML/pdf.html")
     except AttributeError as e:
         logging.error(f"endpoint={request.path} method={request.method}, error={str(e)}")
         return jsonify({'error': str(e)}), 400
